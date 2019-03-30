ba557e1dccb51aeb32b1462ea0329e39345ef3e3
Tiago Bonin <tiagoalencar.b@gmail.com>
2019-03-30 14:36:13 +0100

    part 4 done

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,70 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// RAM[24576] is updated with keyboard keys
+
+// set keyboard RAM to 0 just in case
+@24576
+M=0
+
+// jump to black routine if key is pressed
+@24576
+D=M
+@8
+D;JNE
+// loop check again
+@2
+0;JMP
+
+// initializes black loop
+@16384
+D=A
+@0
+M=D
+// loop to black the screen
+@0
+D=M
+A=D
+M=-1
+
+// if key is not pressed anymore white screen
+@24576
+D=M
+@29
+D;JEQ
+
+// increments position to fill with black
+@0
+M=M+1
+
+//validate if next memory address is still screen
+D=M
+@24576
+D=D-A
+@12
+D;JLT
+@2
+0;JMP
+
+// loop to white the screen
+@16384
+D=A
+@0
+M=D
+
+@0
+D=M
+A=D
+M=-1
+M=!M
+
+@0
+M=M+1
+
+D=M
+@24576
+D=D-A
+@33
+D;JLT
+@2
+0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,51 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// set RAM[2] to 0
+@2
+M=0
+
+// jump to end if any is 0
+@0
+D=M
+@21
+D;JEQ
+@1
+D=M
+@21
+D;JEQ
+
+// set RAM[2] to D
+@2
+D=M
+
+// set RAM[0] to M
+@0
+
+// sum D with the value of RAM[0]
+D=D+M
+
+// store the value of D in RAM[2]
+@2
+M=D
+
+// subtract 1 from RAM[1]
+@1
+M=M-1
+
+// set M to D
+D=M
+
+// set M to begin of program
+@10
+
+D;JGT
+
+@21
+0;JMP
+
+
+
+
+
